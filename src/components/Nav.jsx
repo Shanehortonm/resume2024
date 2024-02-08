a838568933363ad3741616e6d3fd9f51da5b46c4
Shanehortonm <shanehortonm@gmail.com>
2024-02-08 04:52:56 -0500

    work on nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import { FaTimes } from "react-icons/fa";
+import { CiMenuFries } from "react-icons/ci";
 
 const Nav = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
   const content = (
     <>
       <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
@@ -40,7 +48,7 @@ const Nav = () => {
     <nav>
       <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4 flex-1">
         <div className="flex items-center flex-1">
-          <span className="text-3xl font-bold">Logo</span>
+          <span className="text-3xl font-bold">Welcome</span>
         </div>
         <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
           <div className="flex-10">
@@ -73,6 +81,10 @@ const Nav = () => {
             </ul>
           </div>
         </div>
+        <div>{click && content}</div>
+        <button className="block sm:hidden transition" onClick={handleClick}>
+          {click ? <FaTimes /> : <CiMenuFries />}
+        </button>
       </div>
     </nav>
   );
